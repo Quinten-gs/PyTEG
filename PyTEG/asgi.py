31ae06ee6e07ac1c2ca978ac9ad2c7e93f7f5864
Quinten Goens <qgoens@gmail.com>
2021-04-08 23:37:01 +0200

    bugfix

--- PyTEG/asgi.py
+++ PyTEG/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pyteg_django.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PyTEG.settings')
 
 application = get_asgi_application()
